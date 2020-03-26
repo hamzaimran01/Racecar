281e4a980dfdacb6bf11039a2d1f895507b1bbf1
hamza <hamzaimran01@gmail.com>
2020-03-26 11:24:33 +0100

    updated extended kalman

--- kalman.py
+++ kalman.py
@@ -46,14 +46,23 @@ s_plot_est=[]
 n_plot_est=[]
 alpha_plot_est=[]
 velocity_plot_est=[]
-model_error_plot_est=[]
+model_error_plot_est_1=[]
+model_error_plot_est_2=[]
+model_error_plot_est_3=[]
+
+
+model_error_plot_true_1=[]
+model_error_plot_true_2=[]
+model_error_plot_true_3=[]
+
+
+
 
 
 g_1=MX.sym('g_1')
 g_2=MX.sym('g_2')
 g_3=MX.sym('g_3')
-g_4=MX.sym('g_4')
-g=vertcat(g_1,g_2,g_3,g_4)
+g=vertcat(g_1,g_2,g_3)
 
 
 
@@ -78,23 +87,22 @@ sdota=(v*np.cos(alpha+C1*derDelta))/(1-kapparef[0]*n)
 
 
 # model in race track coordinates
-xdot = vertcat(sdota,\
-v*sin(alpha+C1*derDelta),\
-v*C2*derDelta-kapparef[0]*sdota,\
+xdot = vertcat(sdota+g_1,\
+v*sin(alpha+C1*derDelta)+g_2,\
+v*C2*derDelta-kapparef[0]*sdota+g_3,\
 Fxd/m*cos(C1*derDelta),\
-g)
+0,\
+0,\
+0)
 
 
 
-ydot=vertcat(vertcat(sdota,\
-v*sin(alpha+C1*derDelta),\
-v*C2*derDelta-kapparef[0]*sdota),\
-g)
 
-N=100#preditction horizon(nr.ofsteps)
+
+N=250#preditction horizon(nr.ofsteps)
 M=1#1 rk4 step
 #dT sampling time
-T=1
+T=4
 DT=T/N/M
 #################################################function for rk4###########################################################
 #to do function for jacobian!
@@ -103,154 +111,155 @@ f_rk4=Function('f_rk4',[x,u],[xdot])
 xk_jac=jacobian(xdot,x)
 xk_jac_val=Function('f_jac_val',[x,u],[xk_jac])
 
-yk_jac=jacobian(ydot,x)
-#print(yk_jac.size())
-yk_jac_val=Function('yk_jac_val',[x,u],[yk_jac])
-#print(yk_jac_val)
 
 
 
-mean,sigma=0,0.5
+d_value=0.025
 
 #initial conditions,U0 impemented directly in the controls
-#X0=[0.1,0.1,0.1,6]#chose the value of 6 because it is giving better results for the covariance matrix
-X0=[0.1,0.1,0.1,6,\
-np.random.normal(mean,sigma,1),\
-np.random.normal(mean,sigma,1),\
-np.random.normal(mean,sigma,1),\
-np.random.normal(mean,sigma,1)]
-U0=[K*np.cos(omega*DT),0]
+
+X_initial=[0.1,0.1,0.1,1,\
+0,\
+0,\
+0,\
+]
+#U0=[K*np.cos(omega*DT),0.1]
 
 #P_model_cov=diag([1,1,1,1])#3rd diagonal term changed to 1 from 0 because otherwise its inverse twill turn into infinity
-P_model_cov=diag([1,1,1,1,1,1,1,1])
+P_model_cov=diag([1,1,1,1,1,1,1])
+P_model_cov_true=diag([1,1,1,1,1,1,1])
+#P_model_cov_est=diag([1,1,1,1,1,1,1])
 
-Wk=np.random.normal(mean,sigma,1)
-Vk=np.random.normal(mean,sigma,1)
-#print(Wk)
-#Wk=Wk*diag([1,1,1,1])
-Wk=Wk*diag([1,1,1,1,1,1,1,1])
-#Vk=Vk*diag([1,1,1])
-Vk=Vk*diag([1,1,1,1,1,1,1])
-#print(Wk)
-s_plot=np.append(s_plot,X0[0])
-n_plot=np.append(n_plot,X0[1])
-alpha_plot=np.append(alpha_plot,X0[2])
-velocity_plot=np.append(velocity_plot,X0[3])
-s_plot_est=np.append(s_plot_est,X0[0])
-n_plot_est=np.append(n_plot_est,X0[1])
-alpha_plot_est=np.append(alpha_plot_est,X0[2])
-velocity_plot_est=np.append(velocity_plot_est,X0[3])
+
+W=0.025
+W_cov=W*diag([1,1,1,1,1,1,1])
+V=0.025
+V_cov=V*diag([1,1,1])
+
+X_est =[0,0,0,0,0,0,0]
+X_values=X_initial
+X_true =X_initial
+
+
+s_plot=np.append(s_plot,X_true[0])
+n_plot=np.append(n_plot,X_true[1])
+alpha_plot=np.append(alpha_plot,X_true[2])
+velocity_plot=np.append(velocity_plot,X_true[3])
+s_plot_est=np.append(s_plot_est,X_est[0])
+n_plot_est=np.append(n_plot_est,X_est[1])
+alpha_plot_est=np.append(alpha_plot_est,X_est[2])
+velocity_plot_est=np.append(velocity_plot_est,X_est[3])
     
-    
+
+                   
 for i in range(N):
-    #f_jac_val(0.1)    
-    #save for plotting
-    
-    
-    A=xk_jac_val(X0,[K*np.cos(i*omega),0.1])
-    #print(A)
-    k1 = f_rk4(X0, [K*np.cos(i*omega),0.1])
-    k2 = f_rk4(X0 + DT/2 * k1,[K*np.cos(omega*i),0.1] )
-    k3 = f_rk4(X0 + DT/2 * k2, [K*np.cos(i*omega),0.1])
-    k4 = f_rk4(X0 + DT * k3, [K*np.cos(i*omega),0.1])
-    X0=X0+DT/6*(k1 +2*k2 +2*k3 +k4)
-    #print("old values are")
-    #print(X0)
-    s_plot=np.append(s_plot,X0[0])
-    n_plot=np.append(n_plot,X0[1])
-    alpha_plot=np.append(alpha_plot,X0[2])
-    velocity_plot=np.append(velocity_plot,X0[3])
-    
-    P_model_cov=A*P_model_cov*transpose(A)+Wk
-    #print(P_model_cov)
 
+    vk=transpose(np.random.multivariate_normal(np.zeros(3),V_cov,1))
+    wk=transpose(np.random.multivariate_normal(np.zeros(7),W_cov,1))
+    # X_est=X_est
+    
+    A=xk_jac_val(X_est,[K*np.cos(i*omega),0.1])
+    #print(A)
+    k1 = f_rk4(X_true, [K*np.cos(i*omega),0.1])
+    k2 = f_rk4(X_true + DT/2 * k1,[K*np.cos(omega*i),0.1] )
+    k3 = f_rk4(X_true + DT/2 * k2, [K*np.cos(i*omega),0.1])
+    k4 = f_rk4(X_true + DT * k3, [K*np.cos(i*omega),0.1])
+    X_true=X_true+DT/6*(k1 +2*k2 +2*k3 +k4)
+    X_true=X_true+wk
+    #print(X_true)
+    
+    s_plot=np.append(s_plot,X_true[0])
+    n_plot=np.append(n_plot,X_true[1])
+    alpha_plot=np.append(alpha_plot,X_true[2])
+    velocity_plot=np.append(velocity_plot,X_true[3])
+    model_error_plot_true_1=np.append(model_error_plot_true_1,X_true[4])
+    model_error_plot_true_2=np.append(model_error_plot_true_2,X_true[5])
+    model_error_plot_true_3=np.append(model_error_plot_true_3,X_true[6])
+    
+
+    
+    P_model_cov_true=A*P_model_cov_true*transpose(A)+W_cov
+    #print(P_model_cov_true)
+    #ok!
     #innovation steps    
     #s,n and alpha are the measured states!
     
-    C=yk_jac_val(X0,[K*np.cos(i*omega),0.1])
-    #print(C)
+
+    C = np.array([[1, 0, 0 ,0 ,0 ,0 ,0], [0, 1, 0 ,0 ,0 ,0 ,0], [0, 0, 1 ,0 ,0 ,0 ,0]])
+    #C=yk_jac_val(X_true,[K*np.cos(i*omega),0.1])
     #print(Vk.size())  
-    P_model_cov=inv(inv(P_model_cov)+mtimes(mtimes(transpose(C),inv(Vk)),C))
+    P_model_cov_est=inv(inv(P_model_cov_true)+mtimes(mtimes(transpose(C),inv(V_cov)),C))
    # print(P_model_cov)
+
     
-    #wk=np.random.normal(mean,sigma,1)*[1,1,1]
-    wk=np.random.normal(mean,sigma,1)*[1,1,1,1,1,1,1]
-    gk=vertcat(X0[0:3,:],X0[4:8,:])
-    #print(gk)
-    #print(wk)
-    Y0=gk+wk
-    measurement_residual=Y0-gk
+    k1_y = f_rk4(X_est, [K*np.cos(i*omega),0.1])
+    k2_y = f_rk4(X_est + DT/2 * k1_y,[K*np.cos(omega*i),0.1] )
+    k3_y = f_rk4(X_est + DT/2 * k2_y, [K*np.cos(i*omega),0.1])
+    k4_y = f_rk4(X_est + DT * k3_y, [K*np.cos(i*omega),0.1])
+    X_est=X_est+DT/6*(k1_y +2*k2_y +2*k3_y +k4_y)
+    #print(X_values)
+
+    #check please
+    Y_meas=X_true[0:3,:]+vk
+    Ck_gk=np.dot(C,X_est)
+    #print(Ck_gk)
+    
+    #print(vk.shape)
+    measurement_residual=Y_meas-Ck_gk
     #print(measurement_residual)
-    X0=X0+mtimes(mtimes(mtimes(P_model_cov,transpose(C)),inv(Vk)),measurement_residual)
-    #print("new values are")
-    #print(X0)
-    #print(mtimes(mtimes(mtimes(P_model_cov,transpose(C)),inv(Vk)),measurement_residual))
-    s_plot_est=np.append(s_plot_est,X0[0])
-    n_plot_est=np.append(n_plot_est,X0[1])
-    alpha_plot_est=np.append(alpha_plot_est,X0[2])
-    velocity_plot_est=np.append(velocity_plot_est,X0[3])
-    model_error_plot_est=np.append(model_error_plot_est,X0[6])
+    
+    X_est=X_est+mtimes(mtimes(mtimes(P_model_cov_est,transpose(C)),inv(V_cov)),measurement_residual) 
+    #print(X_est)
+    
+    
+    s_plot_est=np.append(s_plot_est,X_est[0])
+    n_plot_est=np.append(n_plot_est,X_est[1])
+    alpha_plot_est=np.append(alpha_plot_est,X_est[2])
+    velocity_plot_est=np.append(velocity_plot_est,X_est[3])
+    model_error_plot_est_1=np.append(model_error_plot_est_1,X_est[4])
+    model_error_plot_est_2=np.append(model_error_plot_est_2,X_est[5])
+    model_error_plot_est_3=np.append(model_error_plot_est_3,X_est[6])
+    
  
-   
- 
-
-""" plt.figure()        
-plt.subplots(221)
-#print(range(N))
-#print(s_plot)
-plt.ylabel('s_plot')
-plt.plot(range(N+1),s_plot,'r--')
-plt.plot(range(N+1),s_plot_est,'g--')
-
-plt.subplot(222)
-plt.ylabel('n_plot')
-plt.plot(range(N+1),n_plot,'r--')
-plt.plot(range(N+1),n_plot_est,'g--')
-plt.subplot(223)
-plt.ylabel('alpha_plot')
-plt.plot(range(N+1),alpha_plot,'r--')
-plt.plot(range(N+1),alpha_plot_est,'g--')
-plt.subplot(224)
-plt.ylabel('velocity_plot')
-plt.plot(range(N+1),velocity_plot,'r--')
-plt.plot(range(N+1),velocity_plot_est,'g--')
 
 
-#plt.plot      
-plt.ylabel('model_error_plot')
-plt.plot(range(N),model_error_plot_est,'r--')
-#plt.plot(range(N+1),velocity_plot_est,'g--')
-plt.show()
- """  
+
 ax1 = plt.subplot2grid(shape=(2,6), loc=(0,0), colspan=2)
 plt.ylabel('s_plot')
-plt.plot(range(N+1),s_plot,'r--')
-plt.plot(range(N+1),s_plot_est,'g--')
+plt.plot(range(N+1),s_plot,'r')
+plt.plot(range(N+1),s_plot_est,'g')
 
 ax2 = plt.subplot2grid((2,6), (0,2), colspan=2)
 plt.ylabel('n_plot')
-plt.plot(range(N+1),n_plot,'r--')
-plt.plot(range(N+1),n_plot_est,'g--')
+plt.plot(range(N+1),n_plot,'r')
+plt.plot(range(N+1),n_plot_est,'g')
 
 ax3 = plt.subplot2grid((2,6), (0,4), colspan=2)
 plt.ylabel('alpha_plot')
-plt.plot(range(N+1),alpha_plot,'r--')
-plt.plot(range(N+1),alpha_plot_est,'g--')
+plt.plot(range(N+1),alpha_plot,'r')
+plt.plot(range(N+1),alpha_plot_est,'g')
 
 ax4 = plt.subplot2grid((2,6), (1,1), colspan=2)
 plt.ylabel('velocity_plot')
-plt.plot(range(N+1),velocity_plot,'r--')
-plt.plot(range(N+1),velocity_plot_est,'g--')
+plt.plot(range(N+1),velocity_plot,'r')
+plt.plot(range(N+1),velocity_plot_est,'g')
 
-ax5 = plt.subplot2grid((2,6), (1,3), colspan=2)
-plt.ylabel('model_error_plot')
-plt.plot(range(N),model_error_plot_est,'r--')
-#plt.plot(range(N+1),velocity_plot_est,'g--')
 
+plt.show()  
+
+ax1 = plt.subplot2grid(shape=(2,6), loc=(0,0), colspan=2)
+plt.ylabel('model_error_plot_1')
+plt.plot(range(N),model_error_plot_true_1,'r')
+plt.plot(range(N),model_error_plot_est_1,'g')
+
+ax2 = plt.subplot2grid((2,6), (0,2), colspan=2)
+plt.ylabel('model_error_plot_2')
+plt.plot(range(N),model_error_plot_true_2,'r')
+plt.plot(range(N),model_error_plot_est_2,'g')
+
+
+ax3 = plt.subplot2grid((2,6), (0,4), colspan=2)
+plt.ylabel('model_error_plot_3')
+plt.plot(range(N),model_error_plot_true_3,'r')
+plt.plot(range(N),model_error_plot_est_3,'g')
 plt.show()
-
- 
-
-
-
-  
